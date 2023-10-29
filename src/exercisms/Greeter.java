fdb68bdc7e6e5e019841fde927f79b45d2d9759b
Allan <allandrs@gmail.com>
2023-10-29 10:52:31 -0300

    Finalizado Hello World e inicio da lasanha

--- src/exercisms/Greeter.java
+++ src/exercisms/Greeter.java
@@ -1,8 +1,11 @@
-import exercisms.Greeter;
-
-public class Main {
+package exercisms;
+public class Greeter {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
         System.out.println(greeter.getGreeting());
+    }
+
+    public String getGreeting() {
+        return "Hello, World!";
     }
 }
---
+++ src/exercisms/Lasagna.java
@@ -0,0 +1,4 @@
+package exercisms;
+
+public class Lasagna {
+}
