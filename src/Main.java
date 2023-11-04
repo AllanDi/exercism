d3feefac04db5cb070621a8d923d91b6cb835bf2
Allan <allandrs@gmail.com>
2023-11-04 02:17:47 -0300

    termino do clean

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,11 @@ public class Main {
 
         System.out.println(SqueakyClean.clean("my    Id"));
         System.out.println(SqueakyClean.clean("my\0Id"));
-        System.out.println(SqueakyClean.clean("à-ḃç"));
-        System.out.println(SqueakyClean.clean("àḃç"));
+        System.out.println(SqueakyClean.clean("à-1ḃç"));
+        System.out.println(SqueakyClean.clean("9 -abcĐ\uD83D\uDE00ω\0"));
 
-        System.out.println("-------------------");
-        System.out.println(SqueakyClean.clean("abc-abc"));
+
+
 
 
 
--- src/exercisms/chars/SqueakyClean.java
+++ src/exercisms/chars/SqueakyClean.java
@@ -1,49 +1,47 @@
 package exercisms.chars;
+
+import java.util.regex.Pattern;
+
 public class SqueakyClean {
     public static String clean(String identifier) {
-       String result = identifier
-               .replace(" ", "_")
-               .replaceAll("\\p{Cntrl}", "CTRL");
+        String result = identifier
+                .replaceAll("[α-ω]", "")
+                .replace(" ", "_")
+                .replaceAll("\\p{Cntrl}", "CTRL")
+                .replaceAll("[\\p{So}]", "")
+                .replaceAll("\\d", "")
+                .replaceAll("\0", "CTRL");
 
-       if (identifier.contains("-")) {
+        String noDigit = "";
 
-           String[] partes = identifier.split("-");
-           char[] caracteres = partes[1].toCharArray();
+        if (identifier.contains("-")) {
+            String[] parts = identifier.split("-");
+            if (Character.isDigit(parts[1].charAt(0))) {
+                noDigit = parts[1].substring(1);
 
-           if (!Character.isDigit(caracteres[0])) {
-               caracteres[0] = Character.toUpperCase(caracteres[0]);
-               String string = new String(caracteres);
-               return partes[0]+string;
-           } else {
+                String resultIf = parts[0] + noDigit.substring(0, 1).toUpperCase() + noDigit.substring(1);
+                String resultIf2 = resultIf
+                        .replaceAll("[α-ω]", "")
+                        .replace(" ", "_")
+                        .replaceAll("\\p{Cntrl}", "CTRL")
+                        .replaceAll("[\\p{So}]", "")
+                        .replaceAll("\\d", "")
+                        .replaceAll("\0", "CTRL");
 
-               return partes[0]+string;
-
-           }
-
-           return partes[0]+string;
-       }
-
-       return result;
+                return resultIf2;
+            } else {
+                String resultIfElse = parts[0] + parts[1].substring(0, 1).toUpperCase() + parts[1].substring(1);
+                String resultIfElse2 = resultIfElse
+                        .replaceAll("[α-ω]", "")
+                        .replace(" ", "_")
+                        .replaceAll("\\p{Cntrl}", "CTRL")
+                        .replaceAll("[\\p{So}]", "")
+                        .replaceAll("\\d", "")
+                        .replaceAll("\0", "CTRL");
+                return resultIfElse2;
+            }
+        }
+        return result;
     }
-
-    public static String teste(String testeString) {
-
-
-
-        String[] partes = testeString.split("-");
-        char[] caracteres = partes[1].toCharArray();
-        caracteres[0] = Character.toUpperCase(caracteres[0]);
-        String string = new String(caracteres);
-
-        System.out.println(string);
-
-        return testeString;
-
-
-
-
-
-
-    }
-
 }
+
