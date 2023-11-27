e02407772d69014fcb60ec7d7d9b81f3830ee3ec
Allan <allandrs@gmail.com>
2023-11-26 22:18:04 -0300

    ending LanguageList

---
+++ src/exercisms/switchStatements/FootballMatchReports.java
@@ -0,0 +1,7 @@
+package exercisms.switchStatements;
+
+public class FootballMatchReports {
+    public static String onField(int shirtNum) {
+        throw new UnsupportedOperationException("Please implement the (static) FootballMatchReports.onField() method");
+    }
+}
