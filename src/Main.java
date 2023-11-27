31a358c61be35b89591ea5d4b856886de2b6463e
Allan <allandrs@gmail.com>
2023-11-26 22:13:33 -0300

    ending LanguageList

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,20 @@ import exercisms.lists.LanguageList;
 public class Main {
     public static void main(String[] args) {
 
-
         LanguageList languageList = new LanguageList();
         languageList.addLanguage("ingles");
         System.out.println(languageList.isEmpty());
-
-
-
-
+        languageList.removeLanguage("ingles");
+        System.out.println(languageList.isEmpty());
+        languageList.addLanguage("ingles");
+        System.out.println(languageList.firstLanguage());
+        System.out.println(languageList.count());
+        System.out.println(languageList.containsLanguage("ingles"));
+        System.out.println(languageList.containsLanguage("java"));
+        System.out.println(languageList.isExciting());
+        languageList.addLanguage("Kotlin");
+        System.out.println(languageList.isExciting());
+        languageList.addLanguage("Java");
+        System.out.println(languageList.isExciting());
     }
 }
--- src/exercisms/lists/LanguageList.java
+++ src/exercisms/lists/LanguageList.java
@@ -4,10 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LanguageList {
+
+    public static void main(String[] args) {
+
+        LanguageList languageList = new LanguageList();
+        languageList.addLanguage("ingles");
+        System.out.println(languageList.isEmpty());
+        languageList.removeLanguage("ingles");
+        System.out.println(languageList.isEmpty());
+        languageList.addLanguage("ingles");
+        System.out.println(languageList.firstLanguage());
+        System.out.println(languageList.count());
+        System.out.println(languageList.containsLanguage("ingles"));
+        System.out.println(languageList.containsLanguage("java"));
+        System.out.println(languageList.isExciting());
+        languageList.addLanguage("Kotlin");
+        System.out.println(languageList.isExciting());
+        languageList.addLanguage("Java");
+        System.out.println(languageList.isExciting());
+    }
+
     private final List<String> languages = new ArrayList<>();
 
     public boolean isEmpty() {
-
         boolean empty = false;
         if (languages.size() == 0) {
             return empty =true;
@@ -16,26 +35,77 @@ public class LanguageList {
     }
 
     public void addLanguage(String language) {
-
+        languages.add(language);
     }
 
     public void removeLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the removeLanguage() method");
+        languages.remove(language);
     }
 
     public String firstLanguage() {
-        throw new UnsupportedOperationException("Please implement the firstLanguage() method");
+        return languages.get(0);
     }
 
     public int count() {
-        throw new UnsupportedOperationException("Please implement the count() method");
+        return languages.size();
     }
 
     public boolean containsLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the containsLanguage() method");
+        for (String e:languages ){
+            if (e == language){
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean isExciting() {
-        throw new UnsupportedOperationException("Please implement the isExciting() method");
+        for (String e:languages ){
+            if (e == "Kotlin" || e == "Java" ){
+                return true;
+            }
+        }
+        return false;
     }
+
+
+    /*
+    import java.util.ArrayList;
+import java.util.List;
+
+public class LanguageList {
+    private final List<String> languages = new ArrayList<>();
+
+    public boolean isEmpty() {
+        return languages.isEmpty();
+    }
+
+    public void addLanguage(String language) {
+        languages.add(language);
+    }
+
+    public void removeLanguage(String language) {
+        languages.remove(language);
+    }
+
+    public String firstLanguage() {
+        return languages.get(0);
+    }
+
+    public int count() {
+        return languages.size();
+    }
+
+    public boolean containsLanguage(String language) {
+        return languages.contains(language);
+    }
+
+    public boolean isExciting() {
+        return languages.contains("Java") || languages.contains("Kotlin");
+    }
+}
+     */
+
+
+
 }
