885f7aa89357cea088a71a5883c605ca041a0242
Allan <allandrs@gmail.com>
2023-12-02 20:05:30 -0300

    ending FootballMatchReports

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,13 @@
 import exercisms.arrays.BirdWatcher;
 import exercisms.lists.LanguageList;
+import exercisms.switchStatements.FootballMatchReports;
 
 public class Main {
     public static void main(String[] args) {
 
-        LanguageList languageList = new LanguageList();
-        languageList.addLanguage("ingles");
-        System.out.println(languageList.isEmpty());
-        languageList.removeLanguage("ingles");
-        System.out.println(languageList.isEmpty());
-        languageList.addLanguage("ingles");
-        System.out.println(languageList.firstLanguage());
-        System.out.println(languageList.count());
-        System.out.println(languageList.containsLanguage("ingles"));
-        System.out.println(languageList.containsLanguage("java"));
-        System.out.println(languageList.isExciting());
-        languageList.addLanguage("Kotlin");
-        System.out.println(languageList.isExciting());
-        languageList.addLanguage("Java");
-        System.out.println(languageList.isExciting());
+        FootballMatchReports footballMatchReports = new FootballMatchReports();
+        System.out.println(FootballMatchReports.onField(1));
+        System.out.println(FootballMatchReports.onField(13));
+
     }
 }
--- src/exercisms/switchStatements/FootballMatchReports.java
+++ src/exercisms/switchStatements/FootballMatchReports.java
@@ -1,7 +1,77 @@
 package exercisms.switchStatements;
 
 public class FootballMatchReports {
-    public static String onField(int shirtNum) {
-        throw new UnsupportedOperationException("Please implement the (static) FootballMatchReports.onField() method");
+
+    public static void main(String[] args) {
+
+        FootballMatchReports footballMatchReports = new FootballMatchReports();
+        System.out.println(FootballMatchReports.onField(1));
+        System.out.println(FootballMatchReports.onField(13));
     }
+
+    public static String onField(int shirtNum) {
+        String result;
+
+        switch (shirtNum) {
+            case 1:
+                result ="goalie";
+                break;
+            case 2:
+                result ="left back";
+                break;
+            case 3:
+            case 4:
+                result ="center back";
+                break;
+            case 5:
+                result ="right back";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                result ="midfielder";
+                break;
+            case 9:
+                result ="left wing";
+                break;
+            case 10:
+                result ="striker";
+                break;
+            case 11:
+                result ="right wing";
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid number");
+        }
+        return result;
+    }
+
+    /*
+
+    public class FootballMatchReports {
+    public static String onField(int shirtNum) {
+        switch (shirtNum) {
+            case 1:
+                return "goalie";
+            case 2:
+                return "left back";
+            case 3: case 4:
+                return "center back";
+            case 5:
+                return "right back";
+            case 6: case 7: case 8:
+                return "midfielder";
+            case 9:
+                return "left wing";
+            case 10:
+                return "striker";
+            case 11:
+                return "right wing";
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+}
+
+     */
 }
