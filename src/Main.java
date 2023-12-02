c0764b4517505d9aa9fda0bc51fdb320c6f9a862
Allan <allandrs@gmail.com>
2023-12-02 20:08:43 -0300

    starting CaptainsLog

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,7 @@ import exercisms.switchStatements.FootballMatchReports;
 public class Main {
     public static void main(String[] args) {
 
-        FootballMatchReports footballMatchReports = new FootballMatchReports();
-        System.out.println(FootballMatchReports.onField(1));
-        System.out.println(FootballMatchReports.onField(13));
+
 
     }
 }
