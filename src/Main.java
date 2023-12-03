6857c9073940801dcea9bf6d81491076182bd0b6
Allan <allandrs@gmail.com>
2023-12-03 09:17:15 -0300

    finishing CaptainsLog

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import exercisms.arrays.BirdWatcher;
 import exercisms.lists.LanguageList;
+import exercisms.randomness.CaptainsLog;
 import exercisms.switchStatements.FootballMatchReports;
+
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-
 
 
     }
--- src/exercisms/randomness/CaptainsLog.java
+++ src/exercisms/randomness/CaptainsLog.java
@@ -2,10 +2,50 @@ package exercisms.randomness;
 
 import java.util.Random;
 
-class CaptainsLog {
+ public class CaptainsLog {
+
+     public static void main(String[] args) {
+
+         CaptainsLog captainsLog = new CaptainsLog(new Random());
+         System.out.println(captainsLog.randomPlanetClass());
+         System.out.println(captainsLog.randomShipRegistryNumber());
+         System.out.println(captainsLog.randomStardate());
+     }
 
     private static final char[] PLANET_CLASSES = new char[]{'D', 'H', 'J', 'K', 'L', 'M', 'N', 'R', 'T', 'Y'};
 
+    private Random random;
+
+    public CaptainsLog(Random random) {
+        this.random = random;
+    }
+
+    public char randomPlanetClass() {
+
+        int randomValue = (random.nextInt(PLANET_CLASSES.length));
+        return PLANET_CLASSES[randomValue];
+
+    }
+
+    public String randomShipRegistryNumber() {
+
+        int randomValue = (random.nextInt(9000)) + 1000;
+        return "NCC-"+randomValue;
+
+    }
+
+    public double randomStardate() {
+
+        double randomValue = (random.nextDouble(1000)) + 41000;
+        return randomValue;
+
+    }
+
+    /*
+    import java.util.Random;
+
+class CaptainsLog {
+    private static final char[] PLANET_CLASSES = new char[]{'D', 'H', 'J', 'K', 'L', 'M', 'N', 'R', 'T', 'Y'};
     private Random random;
 
     CaptainsLog(Random random) {
@@ -13,14 +53,16 @@ class CaptainsLog {
     }
 
     char randomPlanetClass() {
-        throw new UnsupportedOperationException("Please implement the CaptainsLog.randomPlanetClass() method");
+        return PLANET_CLASSES[random.nextInt(10)];
     }
 
     String randomShipRegistryNumber() {
-        throw new UnsupportedOperationException("Please implement the CaptainsLog.randomShipRegistryNumber() method");
+        return "NCC-" + (1000 + random.nextInt(9000));
     }
 
     double randomStardate() {
-        throw new UnsupportedOperationException("Please implement the CaptainsLog.randomStardate() method");
+        return 41000 + 1000 * random.nextDouble();
     }
+}
+     */
 }
