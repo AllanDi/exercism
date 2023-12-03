16755210bcdd8adad1a37f29833f8c29005c419b
Allan <allandrs@gmail.com>
2023-12-03 10:59:29 -0300

    starting AppointmentSchedule

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println(badge.print(null,"joao", "faturamento"));
         System.out.println(badge.print(1,null, "faturamento"));
         System.out.println(badge.print(1,"joao", null));
+        System.out.println(badge.print(null,"joao", null));
 
 
     }
--- src/exercisms/nullability/Badge.java
+++ src/exercisms/nullability/Badge.java
@@ -1,17 +1,44 @@
 package exercisms.nullability;
 
 public class Badge {
+
+    public static void main(String[] args) {
+
+        Badge badge = new Badge();
+
+        System.out.println(badge.print(1,"joao", "faturamento"));
+        System.out.println(badge.print(null,"joao", "faturamento"));
+        System.out.println(badge.print(1,null, "faturamento"));
+        System.out.println(badge.print(1,"joao", null));
+        System.out.println(badge.print(null,"joao", null));
+
+    }
+
+
     public String print(Integer id, String name, String department) {
 
-        if (id == null) {
+        if (department != null && id == null) {
             return name + " - " + department.toUpperCase();
         } else if ( name == null){
             return "[" + id + "] - " + department.toUpperCase();
-        } else if (department == null){
-            return "[" + id + "] - " + name;
+        } else if (department == null && id != null ){
+            return "[" + id + "] - " + name + " - OWNER";
+        } else if (department == null && id == null ){
+            return name + " - OWNER";
         } else {
             return "[" + id + "] - " + name + " - " + department.toUpperCase();
         }
     }
+    /*
+
+    class Badge {
+    public String print(Integer id, String name, String department) {
+        return String.format("%s%s - %s",
+                id != null ? String.format("[%d] - ", id) : "",
+                name,
+                department != null ? department.toUpperCase() : "OWNER");
+    }
+}
+     */
 
  }
