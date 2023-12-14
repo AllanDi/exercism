11f2007247e63cb284e6ab6a526d8e410c94a584
Allan <allandrs@gmail.com>
2023-12-14 13:26:29 -0300

    wip AppointmentSchedule

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,42 @@
-import exercisms.dateTime.AppointmentScheduler;
-import exercisms.nullability.Badge;
 
-import java.time.*;
+import exercisms.dateTime.AppointmentScheduler;
+
+import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
 
-        LocalDate date = LocalDate.of(2007,12,13);
-        LocalDate date1 = LocalDate.of(2007,12,14);
-        LocalDate date2 = LocalDate.of(2007,12,15);
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        LocalDate d01 = LocalDate.now();
+        LocalDateTime d02 = LocalDateTime.now();
+        Instant d03 = Instant.now();
+        LocalDate d04 = LocalDate.parse("2023-12-14");
+        LocalDateTime d05 = LocalDateTime.parse("2023-12-14T12:58:00");
+        Instant d06 = Instant.parse("2023-12-14T12:58:00Z");
+        Instant d07 = Instant.parse("2023-12-14T12:58:00-03:00");
+
+        LocalDate d08 = LocalDate.parse("20/07/2023", fmt1);
+        LocalDateTime d09 = LocalDateTime.parse("20/07/2023 01:30", fmt2);
 
 
-        System.out.println(date.toString());
-        System.out.println(LocalDate.now());
-
-
-
-        System.out.println(date1.isBefore(date2));
-        System.out.println(date2.isBefore(date1));
-        System.out.println(date.getDayOfMonth());
-        System.out.println(date);
+        System.out.println("d01 = " + d01);
+        System.out.println("d02 = " + d02);
+        System.out.println("d03 = " + d03);
+        System.out.println("d04 = " + d04);
+        System.out.println("d05 = " + d05);
+        System.out.println("d06 = " + d06);
+        System.out.println("d07 = " + d07);
+        System.out.println("d08 = " + d08);
+        System.out.println("d09 = " + d09);
 
         AppointmentScheduler scheduler = new AppointmentScheduler();
-        scheduler.schedule("7/25/2019 13:45:00");
 
+        System.out.println(scheduler.schedule("7/25/2019 13:45:00"));
 
 
 
--- src/exercisms/dateTime/AppointmentScheduler.java
+++ src/exercisms/dateTime/AppointmentScheduler.java
@@ -1,14 +1,16 @@
 package exercisms.dateTime;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class AppointmentScheduler {
-    public LocalDateTime schedule(String appointmentDateDescription) {
+    public LocalDateTime schedule(String appointmentDateDescription){
 
-        LocalDateTime.parse(appointmentDateDescription);
-
-
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
+        LocalDateTime d01 = LocalDateTime.parse(appointmentDateDescription, fmt1);
+        return d01;
     }
 
     public boolean hasPassed(LocalDateTime appointmentDate) {
