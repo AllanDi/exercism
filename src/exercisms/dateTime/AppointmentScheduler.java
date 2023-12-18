794df7fad991e7e2a7b3a1c14e7926168f6f8494
Allan <allandrs@gmail.com>
2023-12-17 23:53:09 -0300

    wip AppointmentSchedule

--- src/exercisms/dateTime/AppointmentScheduler.java
+++ src/exercisms/dateTime/AppointmentScheduler.java
@@ -1,26 +1,29 @@
 package exercisms.dateTime;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class AppointmentScheduler {
     public LocalDateTime schedule(String appointmentDateDescription){
-
-        DateTimeFormatter parser = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter parser = DateTimeFormatter.ofPattern("M/dd/yyyy HH:mm:ss");
         LocalDateTime d01 = LocalDateTime.parse(appointmentDateDescription, parser);
-
         return d01;
     }
 
     public boolean hasPassed(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.hasPassed() method");
+        if (appointmentDate.isBefore(LocalDateTime.now())) {
+            return true;
+        }
+        return false;
     }
 
     public boolean isAfternoonAppointment(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.isAfternoonAppointment() method");
+        int hour = appointmentDate.getHour();
+        if(hour >=12 && hour < 18) {
+            return true;
+        }
+        return false;
     }
 
     public String getDescription(LocalDateTime appointmentDate) {
