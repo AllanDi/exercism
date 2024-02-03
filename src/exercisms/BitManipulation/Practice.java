7a28ef7ca9e5e992d8b9fd61d50b162855bab720
Allan <allandrs@gmail.com>
2024-02-03 10:36:59 -0300

    starting Secrets exercise

---
+++ src/exercisms/BitManipulation/Practice.java
@@ -0,0 +1,19 @@
+package exercisms.BitManipulation;
+
+import static exercisms.BitManipulation.Secrets.shiftBack;
+
+public class Practice {
+    public static void main(String[] args) {
+
+        System.out.println(0b1001);
+        System.out.println(0b0010);
+
+        System.out.println(shiftBack(0b1001, 2));
+
+
+
+    }
+
+
+
+}
--- src/exercisms/BitManipulation/Secrets.java
+++ src/exercisms/BitManipulation/Secrets.java
@@ -1,4 +1,20 @@
 package exercisms.BitManipulation;
 
 public class Secrets {
+    public static int shiftBack(int value, int amount) {
+
+        return amount == 2 ? value >> amount : value >>> amount;
+    }
+
+    public static int setBits(int value, int mask) {
+        return value & mask;
+    }
+
+    public static int flipBits(int value, int mask) {
+        throw new UnsupportedOperationException("Please implement the (static) Secrets.flipBits() method");
+    }
+
+    public static int clearBits(int value, int mask) {
+        throw new UnsupportedOperationException("Please implement the (static) Secrets.clearBits() method");
+    }
 }
